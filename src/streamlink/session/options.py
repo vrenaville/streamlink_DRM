5e4bea5d6ce638c46ff3db5565af09c36c11cd17
Anemone •⩊• <96365455+L3m0n-Cao@users.noreply.github.com>
2024-08-30 16:58:37 +1000

    add most stuff

--- src/streamlink/session/options.py
+++ src/streamlink/session/options.py
@@ -302,6 +302,7 @@ class StreamlinkOptions(Options):
             "ffmpeg-verbose-path": None,
             "ffmpeg-loglevel": None,
             "ffmpeg-fout": None,
+            "ffmpeg-dkey": None,
             "ffmpeg-video-transcode": None,
             "ffmpeg-audio-transcode": None,
             "ffmpeg-copyts": False,
